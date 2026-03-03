d98c4485427177db022b3f86eee237e61b5fd63d
ntwaliheritier <ntwalihatsor78.nh@gmail.com>
2026-03-03 15:25:30 +0200

    create eks stack and rename infra.ts to main.ts

--- infra/bin/main.ts
+++ infra/bin/main.ts
@@ -2,8 +2,13 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { NetworkStack } from '../lib/network-stack';
 import { EcrStack } from '../lib/ecr-stack';
+import { EksStack } from '../lib/eks-stack';
 
 const app = new cdk.App();
 
 const networkStack = new NetworkStack(app, 'NetworkStack');
 const ecrStack = new EcrStack(app, 'EcrStack');
+new EksStack(app, 'EksStack', {
+  vpc: networkStack.vpc,
+  repository: ecrStack.repository,
+});
