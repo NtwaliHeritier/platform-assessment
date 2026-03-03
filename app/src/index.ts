36cf50a238999401d77fb34a57fed6e4c545e6cb
ntwaliheritier <ntwalihatsor78.nh@gmail.com>
2026-03-03 21:50:07 +0200

    add /metrics endpoint

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,7 +1,17 @@
 import express, { type Request, type Response } from 'express';
+import client from "prom-client";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const register = new client.Registry();
+
+client.collectDefaultMetrics({ register });
+
+app.get('/metrics', async (_req, res: Response) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 app.get('/health', (req: Request, res: Response) => {
     res.json({status: 'ok'});
