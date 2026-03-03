45ca4f11aced5ff0fa507d8fbe939be3200d7e14
ntwaliheritier <ntwalihatsor78.nh@gmail.com>
2026-03-03 15:11:17 +0200

    add ecr stack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { NetworkStack } from '../lib/network-stack';
+import { EcrStack } from '../lib/ecr-stack';
 
 const app = new cdk.App();
 
 const networkStack = new NetworkStack(app, 'NetworkStack');
+const ecrStack = new EcrStack(app, 'EcrStack');
---
+++ infra/lib/ecr-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+
+export class EcrStack extends cdk.Stack {
+  public readonly repository: ecr.Repository;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.repository = new ecr.Repository(this, 'AppRepository', {
+      repositoryName: 'app-repository',
+      imageScanOnPush: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+  }
+}
